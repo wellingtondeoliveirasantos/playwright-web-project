54526a9b2b3e7afcd66b378b54e2eaae34d85860
Wellington Santos <wellingtondeoliveirasanto@gmail.com>
2025-08-11 22:01:38 -0300

    ...

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,18 @@
+console.log('Variável CI no config:', process.env.CI);
+
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: process.env.CI ? 60000 : 30000, // Mais tempo no CI
+  timeout: process.env.CI ? 60000 : 30000,
   retries: 1,
   use: {
     headless: process.env.CI ? true : false,
     viewport: { width: 1280, height: 960 },
     actionTimeout: 10000,
     baseURL: 'https://automationexercise.com',
-    screenshot: 'only-on-failure', // Guarda screenshot no erro
-    video: 'retain-on-failure',    // Guarda vídeo no erro
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
   workers: process.env.CI ? 2 : undefined,
   reporter: process.env.CI ? 'line' : 'list',
