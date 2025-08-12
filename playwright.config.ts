8024c9b494808d40a1c7c5095eb2395fcdb8ff49
Wellington Santos <wellingtondeoliveirasanto@gmail.com>
2025-08-11 21:48:50 -0300

    Ajustes no CI yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,10 +5,11 @@ export default defineConfig({
   timeout: 30000,
   retries: 1,
   use: {
-    headless: process.env.CI ? true : false, // CI = headless
+    headless: process.env.CI ? true : false, // No CI roda headless
     viewport: { width: 1280, height: 960 },
     actionTimeout: 10000,
     baseURL: 'https://automationexercise.com',
   },
-  workers: process.env.CI ? 2 : undefined, // Menos workers no CI para estabilidade
+  workers: process.env.CI ? 2 : undefined, // Menos workers no CI
+  reporter: process.env.CI ? 'line' : 'list', // Logs mais simples no CI
 });
