afc1cebe5c09ee4d5eb7b5e37439f15ebe426647
Wellington Santos <wellingtondeoliveirasanto@gmail.com>
2025-08-11 21:45:42 -0300

    Ajuste no CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,9 +5,10 @@ export default defineConfig({
   timeout: 30000,
   retries: 1,
   use: {
-    headless: false,
+    headless: process.env.CI ? true : false, // CI = headless
     viewport: { width: 1280, height: 960 },
     actionTimeout: 10000,
     baseURL: 'https://automationexercise.com',
   },
+  workers: process.env.CI ? 2 : undefined, // Menos workers no CI para estabilidade
 });
